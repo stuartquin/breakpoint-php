2dc9263c3cacf98af4a55520794ceb293bbb01b5
Stuart Quin <stuart.quin@gmail.com>
2015-04-30 11:03:11 +0100

    Handle fatal errors

--- src/better_errors/frame.php
+++ src/better_errors/frame.php
@@ -1,20 +1,61 @@
 <?php
 // error handler function
 function FrameErrorHandler($errNum, $errstr, $errfile, $errline) {
-  $errorType = "ERROR";
+  $errorType = null;
+  // print("<pre>");
+  // var_dump("NUM: ".$errNum);
+  // var_dump($errstr);
+  // var_dump($errfile);
+  // var_dump($errline);
+  // var_dump(E_USER_ERROR);
+  // var_dump(E_USER_WARNING);
+  // var_dump(E_USER_NOTICE);
+  // print("</pre>");
+
+
   switch ($errNum) {
     case E_USER_ERROR: $errorType = "ERROR"; break;
     case E_USER_WARNING: $errorType = "WARNING"; break;
     case E_USER_NOTICE: $errorType = "NOTICE"; break;
   }
 
-  Frame::Frame()->except($errorType, $errNum, $errstr, $errline, $errfile);
+  if ($errorType === null) {
+    return FALSE;
+  }
+
+  $trace = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT);
+  Frame::Frame()->except($errorType, $errNum, $errstr, $errline, $errfile, $trace);
+  return TRUE;
 }
+
+
+function FatalErrorHandler() {
+  $errfile = "unknown file";
+  $errstr = "shutdown";
+  $errNum = E_CORE_ERROR;
+  $errline = 0;
+
+  $error = error_get_last();
+
+  if( $error !== NULL) {
+    $errNum  = $error["type"];
+    $errfile = $error["file"];
+    $errline = $error["line"];
+    $errstr  = $error["message"];
+  }
+  $trace = debug_backtrace();
+
+  Frame::Frame()->except("FATAL", $errNum, $errstr, $errline, $errfile, $trace);
+  Frame::Frame()->render();
+  return TRUE;
+}
+
+// register_shutdown_function("FatalErrorHandler");
 set_error_handler("FrameErrorHandler");
 
 class Frame {
   private $frames = array();
-  private $exceptios = array();
+  private $exceptions = array();
 
   static $instance = null;
 
@@ -29,17 +70,22 @@ class Frame {
     return Frame::$instance;
   }
 
-  public function except($errType, $errNum, $message, $lineNum, $fileName) {
-    $trace = debug_backtrace();
-    print("<pre>");
-    // print_r(debug_backtrace());
-    print("</pre>");
+  public function except($errType, $errNum, $message, $lineNum, $fileName, $trace) {
     $this->exceptions = array(
       "type" => $errType ." #".$errNum,
       "message" => $message,
       "path" => $_SERVER["REQUEST_URI"]
     );
-    $this->createFrame($message, $lineNum, $fileName, $trace[2]);
+
+    if ($lineNum ) {
+      // print("<pre>");
+      // var_dump($message);
+      // var_dump($trace);
+      // print("</pre>");
+    }
+
+    $trace = $trace[min(count($trace) - 1, 2)];
+    $this->createFrame($message, $lineNum, $fileName, $trace);
   }
 
   public function createFrame($message, $lineNum, $fileName, $trace) {
@@ -60,15 +106,18 @@ class Frame {
     if (isset($trace["object"])) {
       $frame["instance"] = get_object_vars($trace["object"]);
       $frame["class_name"] = $trace["class"];
-      $frame["method_name"] = $trace["function"];
     } else {
       $frame["instance"] = array();
-      $frame["class_name"] = "";
+      $frame["class_name"] = null;
+    }
+
+    if (isset($trace["function"])) {
+      $frame["method_name"] = $trace["function"];
+    } else {
       $frame["method_name"] = "";
     }
 
     $this->frames[] = $frame;
-
   }
 
   public function inspect($local) {
@@ -79,15 +128,18 @@ class Frame {
     );
 
     $debugTrace = debug_backtrace();
-    $trace = $debugTrace[1];
+    $trace = $debugTrace[min(count($debugTrace) - 1, 2)];
     $trace["local"] = $local;
     $this->createFrame("Frame", $debugTrace[0]["line"], $debugTrace[0]["file"], $trace);
   }
 
   public function render() {
-    global $GlobalFramegerFrames, $GlobalFramegerException;
-    $GlobalFramegerFrames = $this->frames;
-    $GlobalFramegerException = $this->exceptions;
-    include("debug_template.php");
+    if (count($this->frames) > 0) {
+      global $GlobalDebuggerFrames, $GlobalDebuggerException;
+      $GlobalDebuggerFrames = $this->frames;
+      $GlobalDebuggerException = $this->exceptions;
+      include("debug_template.php");
+      exit;
+    }
   }
 }
